f6f09fc3de0236cbe3d39f5bbf5dd2e49e0fa0d5
武治含 <wzh@wuzhihandeMacBook-Pro.local>
2018-06-23 11:38:35 +0800

    I added some components

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 function formateDate(date){
   return date.toLocaleDateString();
 }
+function Avatar(props){
+  return(
+  <img className="Avatar"
+  src={props.user.avatarUrl}
+  alt={props.user.name}/>
+  );
+}
+function UserInfo(props){
+  return(
+    <div className="UserInfo">
+        <Avatar user={props.user} />
+        <div className="UserInfo-name">
+          {props.user.name}
+        </div>
+    </div>
+  );
+}
 function Comment(props){
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img className="Avatar"
-          src={props.author.avatarUrl} 
-          alt={props.author.name} />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo user={props.author} />
       <div className="Comment-text">
         {props.text}
       </div>
