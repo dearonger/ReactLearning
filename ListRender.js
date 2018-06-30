7ebd64f49c19e2c909300fdc9dacf7610cc0a6f5
dearonger <wuzhihan_1@163.com>
2018-06-30 22:44:10 +0800

    map() function and ListItem component

--- ListRender.js
+++ ListRender.js
@@ -1,17 +1,21 @@
-function NumberList(props){
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
+function NumberList(props) {
   const numbers = props.numbers;
-  const listItem = numbers.map(number =>
-     <li key={number.toString()}>
-       {number}
-     </li>
-  );
-  return(
-    <ul>{listItem}</ul>
+  return (
+    <ul>
+      {numbers.map((number) =>
+        <ListItem key={number.toString()}
+                  value={number} />
+      )}
+    </ul>
   );
 }
 
 const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-    <NumberList  numbers={numbers}/>,
-    document.getElementById('root')
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
 );
