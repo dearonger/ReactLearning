1e2288cd76dfef622d87fffdbca0d06e2bfb0623
dearonger <wuzhihan_1@163.com>
2018-07-05 16:36:10 +0800

    class general purpose component

--- childComponents.js
+++ childComponents.js
@@ -15,19 +15,41 @@ function FancyBorder(props) {
             <p className="Dialog-message">
                 {props.message}
             </p>
+            {props.children}
           </FancyBorder>
       )
   }
   
-  function WelcomeDialog() {
-    return (
-      <Dialog 
-      title="Welcome" 
-      message="Thank you for visiting our spacecraft!" />
-    );
+  class SignUpDialog extends React.Component{
+    constructor(props){
+      super(props);
+      this.state = {login:''};
+      this.handleChange=this.handleChange.bind(this);
+      this.handleSignUp=this.handleSignUp.bind(this);
+    }
+    handleChange(e){
+      this.setState({login: e.target.value});
+    }
+    handleSignUp(){
+      alert(`Welcome aboard, ${this.state.login}!`);
+    }
+    render(){
+      return(
+        <Dialog
+        title="Mars Exploration Program"
+        message="How should we refer to you?" >
+        <div>
+          <input type="text" onChange={this.handleChange} value={this.state.login} />
+          <button onClick={this.handleSignUp}>
+            Sign Me Up!
+          </button>
+        </div>
+        </Dialog>
+      );
+    }
   }
-  
+
   ReactDOM.render(
-    <WelcomeDialog />,
+    <SignUpDialog />,
     document.getElementById('root')
   );
